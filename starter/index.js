05fa200218ec536d10fec8a29fc80f222f42964e
Hamza Baig <hamzamunawar345@gmail.com>
2024-06-10 15:04:08 +0500

    update starter/index.js and output.txt

--- starter/index.js
+++ starter/index.js
@@ -5,4 +5,13 @@ const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
 const txtOut = `This is a text added in avocado file: ${textInput}. \n Created on ${Date.now()}`;
 
 fs.writeFileSync("./txt/output.txt", txtOut);
-console.log("File written");
+// console.log("File written");
+
+//Non-Blocking, asynchronous way
+
+fs.readFile("./txt/start.txt", 'utf-8', (err, data1)=>{
+    fs.readFile('./txt/'+ data1 +'.txt', 'utf-8', (err, data2)=>{
+        console.log(data2)
+    })
+})
+
